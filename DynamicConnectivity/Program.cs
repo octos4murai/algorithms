fba58a017db8dabae052cc31559f0360d128c4a2
Al Idian <al.idian@me.com>
2020-04-15 19:43:37 -0700

    Add client for Dynamic Connectivity solutions

---
+++ DynamicConnectivity/Program.cs
@@ -0,0 +1,22 @@
+﻿using QuickFind;
+using System;
+
+namespace DynamicConnectivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nodes = new int[] { 5, 7, 2 };
+            var qf = new QuickFindService(nodes);
+            qf.Union(0, 1);
+            // qf.Union(2, 0);
+
+            // For debugging only
+            // Console.WriteLine($"[{string.Join(", ", qf.Nodes)}]");
+            Console.WriteLine(qf.Find(0, 2));
+        }
+    }
+}
