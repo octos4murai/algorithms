22a64ccb980971dffcfb7e794989e18ff24e6330
Al Idian <al.idian@me.com>
2020-04-17 01:58:25 +0000

    Add unit tests for QuickFind

---
+++ QuickFind.Tests/QuickFindService_QuickFindShould.cs
@@ -0,0 +1,40 @@
+using Xunit;
+
+namespace QuickFind.Tests
+{
+    public class QuickFindService_QuickFindShould
+    {
+        private readonly QuickFindService _qf;
+
+        public QuickFindService_QuickFindShould()
+        {
+            var sampleArr = new int[] { 5, 3, 7, 11, 4, 2, 0 };
+            _qf = new QuickFindService(sampleArr);
+            _qf.Union(0, 6);
+            _qf.Union(0, 1);
+            _qf.Union(5, 2);
+            _qf.Union(2, 3);
+        }
+
+        [Theory]
+        [InlineData(0, 6)]
+        [InlineData(1, 0)]
+        [InlineData(1, 6)]
+        [InlineData(5, 2)]
+        public void Find_ConnectedValues_ReturnTrue(int x, int y)
+        {
+            var result = _qf.Find(x, y);
+            Assert.True(result, $"{x} and {y} should be connected.");
+        }
+
+        [Theory]
+        [InlineData(0, 5)]
+        [InlineData(4, 5)]
+        [InlineData(3, 1)]
+        public void Find_DisconnectedValues_ReturnFalse(int x, int y)
+        {
+            var result = _qf.Find(x, y);
+            Assert.False(result, $"{x} and {y} should not be connected.");
+        }
+    }
+}
