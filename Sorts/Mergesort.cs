afd204666b9aab818e759cfa768b7fd6a5b989a8
Al Idian <al.idian@me.com>
2020-05-12 19:54:38 -0700

    Complete Mergesort implementation with unit tests.

--- Sorts/Mergesort.cs
+++ Sorts/Mergesort.cs
@@ -4,28 +4,55 @@ namespace Sorts
 {
     public class Mergesort<T> where T : IComparable<T>, IEquatable<T>
     {
-        public void Run(T[] arr)
+        public static void Run(T[] arr)
         {
             var aux = new T[arr.Length];
             Sort(arr, aux, 0, arr.Length - 1);
         }
 
-        public void Sort(T[] arr, T[] aux, int lo, int hi)
+        public static void Sort(T[] arr, T[] aux, int lo, int hi)
         {
             if (lo == hi)
                 return;
 
-            int md = (hi - lo) / 2;
+            int md = ((hi - lo) / 2) + lo;
 
             Sort(arr, aux, lo, md);
             Sort(arr, aux, md + 1, hi);
 
-            Merge(arr, aux, lo, hi);
+            Merge(arr, aux, lo, md, hi);
         }
 
-        private void Merge(T[] arr, T[] aux, int lo, int hi)
+        private static void Merge(T[] arr, T[] aux, int lo, int md, int hi)
         {
-            throw new NotImplementedException();
+            Array.Copy(arr, lo, aux, lo, hi - lo + 1);
+
+            int i = lo;
+            int j = md + 1;
+
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > md)
+                {
+                    Array.Copy(aux, j, arr, k, hi - j + 1);
+                    break;
+                }
+                else if (j > hi)
+                {
+                    Array.Copy(aux, i, arr, k, md - i + 1);
+                    break;
+                }
+                else if (aux[i].CompareTo(aux[j]) < 0)
+                {
+                    arr[k] = aux[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = aux[j];
+                    j++;
+                }
+            }
         }
     }
 }
